48e18afaa269a457b099218a0da874382ec4f954
Mekhy W.! <mefkhytheroboticist@gmail.com>
2019-12-08 13:05:10 -0300

    i counter range fixed

--- TIRAMISU/Topographer.py
+++ TIRAMISU/Topographer.py
@@ -26,17 +26,17 @@ def PlotWalls():
         if(lidardata[angle]>0):
             AngCos = math.cos(math.radians(angle+Poser.RobotCompass))
             AngSin = math.sin(math.radians(angle+Poser.RobotCompass))
-            for i in lidardata[angle]*0.1:
-                if(WallMap[Poser.CurrentFloor][round(Poser.RobotPositionX+(i*0.1*AngCos))][round(Poser.RobotPositionY+(i*0.1*AngSin))] == (-1)):
-                    WallMap[Poser.CurrentFloor][round(Poser.RobotPositionX+(i*0.1*AngCos))][round(Poser.RobotPositionY+(i*0.1*AngSin))] = 0
-                elif(WallMap[Poser.CurrentFloor][round(Poser.RobotPositionX+(i*0.1*AngCos))][round(Poser.RobotPositionY+(i*0.1*AngSin))] == 1):
-                    WallMap[Poser.CurrentFloor][round(Poser.RobotPositionX+(i*0.1*AngCos))][round(Poser.RobotPositionY+(i*0.1*AngSin))] = 0
+            for i in range(0, round(lidardata[angle]*0.1)):
+                if(WallMap[Poser.CurrentFloor][round(Poser.RobotPositionX+(i*AngCos))][round(Poser.RobotPositionY+(i*AngSin))] == (-1)):
+                    WallMap[Poser.CurrentFloor][round(Poser.RobotPositionX+(i*AngCos))][round(Poser.RobotPositionY+(i*AngSin))] = 0
+                elif(WallMap[Poser.CurrentFloor][round(Poser.RobotPositionX+(i*AngCos))][round(Poser.RobotPositionY+(i*AngSin))] == 1):
+                    WallMap[Poser.CurrentFloor][round(Poser.RobotPositionX+(i*AngCos))][round(Poser.RobotPositionY+(i*AngSin))] = 0
                     for c in range(-15, 15):
                         for r in range(-15, 15):
                             if(math.sqrt((c*c)+(r*r)) <= 9):
-                                WallSplashMap[Poser.CurrentFloor][round(Poser.RobotPositionX+(i*0.1*AngCos)) + c][round(Poser.RobotPositionY+(i*0.1*AngSin)) + r] -= 1
-                            elif(math.sqrt((c*c)+(r*r)) <= 15 and WallWeightMap[Poser.CurrentFloor][round(Poser.RobotPositionX+(i*0.1*AngCos)) + c][round(Poser.RobotPositionY+(i*0.1*AngSin)) + r] >= 15-math.sqrt((c*c)+(r*r))):
-                                WallWeightMap[Poser.CurrentFloor][round(Poser.RobotPositionX+(i*0.1*AngCos)) + c][round(Poser.RobotPositionY+(i*0.1*AngSin)) + r] -= 15-math.sqrt((c*c)+(r*r))
+                                WallSplashMap[Poser.CurrentFloor][round(Poser.RobotPositionX+(i*AngCos)) + c][round(Poser.RobotPositionY+(i*AngSin)) + r] -= 1
+                            elif(math.sqrt((c*c)+(r*r)) <= 15 and WallWeightMap[Poser.CurrentFloor][round(Poser.RobotPositionX+(i*AngCos)) + c][round(Poser.RobotPositionY+(i*AngSin)) + r] >= 15-math.sqrt((c*c)+(r*r))):
+                                WallWeightMap[Poser.CurrentFloor][round(Poser.RobotPositionX+(i*AngCos)) + c][round(Poser.RobotPositionY+(i*AngSin)) + r] -= 15-math.sqrt((c*c)+(r*r))
             if(WallMap[Poser.CurrentFloor][round(Poser.RobotPositionX+(lidardata[angle]*0.1*AngCos))][round(Poser.RobotPositionY+(lidardata[angle]*0.1*AngSin))] == (-1) or WallMap[Poser.CurrentFloor][round(Poser.RobotPositionX+(lidardata[angle]*0.1*AngCos))][round(Poser.RobotPositionY+(lidardata[angle]*0.1*AngSin))] == 0):
                 WallMap[Poser.CurrentFloor][round(Poser.RobotPositionX+(lidardata[angle]*0.1*AngCos))][round(Poser.RobotPositionY+(lidardata[angle]*0.1*AngSin))] = 1
                 for c in range(-15, 15):
