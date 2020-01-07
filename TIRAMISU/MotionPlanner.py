f2e8090fc16b74b2955cf3b3233990b60f0338f7
Mekhy W.! <mefkhytheroboticist@gmail.com>
2020-01-07 10:38:36 -0300

    Nodes only expand to spaces that are known to be free

--- TIRAMISU/MotionPlanner.py
+++ TIRAMISU/MotionPlanner.py
@@ -44,7 +44,7 @@ class Node:
             self.backtrace_path()
         for c in range(-1, 2):
             for r in range(-1, 2):
-                if(EXISTENT_NODE_MAP[self.position_x+c][self.position_y+r] == 0 and Topographer.WALL_MAP[self.position_x+c][self.position_y+r] in (-1, 0) and Topographer.WALL_SPLASH_MAP[self.position_x+c][self.position_y+r] <= 0 and Topographer.LANDMARK_MAP[self.position_x+c][self.position_y+r] != 99):
+                if(EXISTENT_NODE_MAP[self.position_x+c][self.position_y+r] == 0 and Topographer.WALL_MAP[self.position_x+c][self.position_y+r] == 0 and Topographer.WALL_SPLASH_MAP[self.position_x+c][self.position_y+r] <= 0 and Topographer.LANDMARK_MAP[self.position_x+c][self.position_y+r] != 99):
                     node = Node(self.position_x+c, self.position_y+r, self)
                     UNVISITED_NODE_LIST.append(node)
     def backtrace_path(self):
